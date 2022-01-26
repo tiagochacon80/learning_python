ee66cdef615f9510138d04cf5173174c9047878c
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-25 20:23:46 -0500

    boucle de répétition - for

---
+++ Boucles de répétitions - for/calculer_la_somme_dentiers.py
@@ -0,0 +1,9 @@
+#Calculer de la somme des n premiers entiers
+n = int(input("Entrez un nombre n: "))
+somme = 0
+for i in range(1, n + 1): #(1,2,3...,n)
+    somme = somme + i
+print("La some des n premiers entiers vaut", somme)
+
+
+
