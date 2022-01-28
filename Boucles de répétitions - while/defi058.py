11e43422c7a1388bfc04759197c76b3299089b33
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-27 19:52:16 -0500

    exercices de while

--- Boucles de répétitions - while/defi058.py
+++ Boucles de répétitions - while/defi058.py
@@ -2,8 +2,13 @@
 
 nombre_ordi = 5
 tentatives = 1
+print('Bonjour! Je suis ton ordinateur...Je viens de penser un numéro entre 0 e 10.')
+print('Est-ce que tu serais capable de le deviner? ')
 nombre_utilisateur = int(input("Informez un numéro entre 0 e 10: "))
 while nombre_utilisateur != nombre_ordi:
-    nombre_utilisateur = int(input("Ton numéro n'est pas correct, essayer à nouveau: "))
+    if nombre_utilisateur > nombre_ordi:
+        nombre_utilisateur = int(input('Moins... Essayez à nouveau: '))
+    else:
+        nombre_utilisateur = int(input('Plus... Essayez a nouveau: '))
     tentatives = tentatives + 1
 print("Vous avez reussi après", tentatives, "tentatives, le numéro est ", nombre_ordi, "Félicitation!")
---
+++ Premières commandes/premieres commandes.py
@@ -0,0 +1,24 @@
+'''print(7 + 4)
+print('7' + '4')
+print(7,'4')
+
+print('Defi 01')
+nome = input('Qual o seu nome? ')
+print('Ola ' + nome + '! ' + 'Prazer em te conhecer')
+print()'''
+
+print('Defi 02')
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu em {} de {} de {}'.format(dia, mes, ano))
+
+'''print()
+print('Defi 03')
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite outro numero: '))
+soma = num1 + num2
+print(soma)'''
+
+#nome = input('Digite seu nome: ')
+#print('É um prazer te conhecer, {}!'.format(nome))
