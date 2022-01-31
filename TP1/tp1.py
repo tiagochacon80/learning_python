7d6ab6acb8704c3262a37852c76477c110a9a092
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-30 21:12:30 -0500

    tp1 update

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -1,13 +1,12 @@
 #Travail pratique 1 pour le cours d'introduction à la programmation avec Python
 print("Bienvenue au calculaeur (calculateur santé/sans T)")
-jour = 0
-ajouter_un_jour = 'o'
-somme = 0
-while ajouter_un_jour != 'n':
-    jour = jour + 1
-    print('\nJour {}'.format(jour))
-    dejeune = input("Avez-vous dejeuné ce matin? (o/n) ")
-    alimentation = float(input("Quelle fraction de votre alimentation a consisté de fruits et légumes? "))
-    activite_physique = int(input("Combien de minutes d'activité physique avez-vous effectué? "))
-    somme = somme + activite_physique
-    ajouter_un_jour = input("Voulez-vous ajouter un jour? (o/n) ")
+
+dejeune = input("Avez-vous dejeuné ce matin? (o/n) ")
+alimentation = float(input("Quelle fraction de votre alimentation a consisté de fruits et légumes? "))
+activite_physique = int(input("Combien de minutes d'activité physique avez-vous effectué? "))
+
+
+if alimentation < 0.2 or alimentation > 0.8:
+    print('Vous ne mangez pas équilibré tous les jours! Visez 50% de fruits et légumes.')
+if activite_physique < 30:
+    print('Vous ne faites pas suffisament de soprt! Visez 30 minutes par jour en moyenne.')
