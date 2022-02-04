e0c8684614f7227266689328ae49c97093862c8f
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-03 20:48:27 -0500

    defi 017 module

---
+++ Modules/defi017.py
@@ -0,0 +1,8 @@
+#hypotenuse formule a² + b² = c²
+cathete_grand = int(input("Cathete plus grand: "))
+cathete_petit = int(input("Cathete plus petit: "))
+calcule = (cathete_petit ** 2) + (cathete_grand ** 2)
+hypoteneuse = calcule**(1/2)
+
+print(hypoteneuse)
+
