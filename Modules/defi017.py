421e3bcb18d2525f8c1d5ccbf525432ab587cb77
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-04 19:02:13 -0500

    Defi 017

--- Modules/defi017.py
+++ Modules/defi017.py
@@ -1,5 +1,5 @@
 #Triangle rectangle - hypotenuse formule a² + b² = c²
-print("Exemple 1")
+print("Solution #1")
 ca = float(input("Cathète plus grand: "))
 co = float(input("Cathète plus petit: "))
 hypoteneuse = (co ** 2 + ca ** 2) ** (1/2)
@@ -7,5 +7,9 @@ hypoteneuse = (co ** 2 + ca ** 2) ** (1/2)
 print("L'hypoteneuse ira mesuré {:.2f}".format(hypoteneuse))
 
 print()
-print("Exemple 2")
+print("Solution #2 - module")
+import math
+ca = float(input("Cathète plus grand: "))
+co = float(input("Cathète plus petit: "))
+print("L'hypoteneuse est de {:.2f}".format(math.hypot(ca, co)))
 
