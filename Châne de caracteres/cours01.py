247b47ff7e378fe265a73f5d94d3f72c6d2803ec
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-06 17:22:19 -0500

    Châne de caracteres et méthodes

---
+++ Châne de caracteres/cours01.py
@@ -0,0 +1,13 @@
+phrase = 'Cours en Video python et PROGRAMMATION'
+print(phrase[1:15:2]) #Il commence par le 1, il jusqu'a au 15 et il va sauter deux
+print(phrase.count('o')) #Il compte la quantité de 'o'
+print(len(phrase))
+print(phrase.replace('python', 'Android'))
+print(phrase.find('en')) #Il va indiqué la position de la string
+print(phrase.lower().find('video'))
+print(phrase.upper().find('PROGRAMMATION'))
+print(phrase.split())
+print('#'.join(phrase))
+
+
+
