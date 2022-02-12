23f7c5550af01adb353d9abbc901a2a8339ebbcd
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-11 21:12:19 -0500

    defi 028 - jeu de devinette

--- Conditions - if/defi028_jeux_de_devinette.py
+++ Conditions - if/defi028_jeux_de_devinette.py
@@ -3,7 +3,7 @@ import random
 print("Je vais penser a un numéro entre 0 a 5, essayez de deviner...")
 numero = int(input("A quel numero j'ai pensé? "))
 print("EN TRAITEMENT, VEUILLEZ PATIENTER...")
-numero_ordi = random.randrange(0, 5) #function pour selectionner un nombre aléatoire
+numero_ordi = random.randint(0, 5) #function pour selectionner un nombre aléatoire
 if numero == numero_ordi:
     print("Felicitation!!! Vous avez reussi!!!")
 else:
