85965e20168e0f6749f726011bd78e656eda8458
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-12 18:02:30 -0500

    defi029 - Photo radar

--- Conditions - if/defi029-photo_radar.py
+++ Conditions - if/defi029-photo_radar.py
@@ -1,7 +1,9 @@
 #Un programme qui lit la vitesse d'une voiture et s'elle dépasse la vitesse 80km, affiche un message, disant qu'il doit payer une amende
-vitesse = int(input("Quelle est la vitesse du véhicule? "))
+vitesse = float(input("Quelle est la vitesse du véhicule? "))
+amende = 0
 if vitesse > 80:
     print("ATTENTION!! Vous avez dépassé la limite de vitesse permi qui est de 80km!!!")
-    print("Vous devez payer une amande!")
+    amende = round((vitesse - 80) * 7)
+    print(f"Vous devez payer une amende de {amende}$!")
 else:
     print("Bonne journée, conduisez prudemment")
