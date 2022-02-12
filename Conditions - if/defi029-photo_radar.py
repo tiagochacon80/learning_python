fc6a3cced665ca1e0f0fd5debab76c1cc1577d8b
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-12 17:40:00 -0500

    defi029 - Photo radar

---
+++ Conditions - if/defi029-photo_radar.py
@@ -0,0 +1,2 @@
+#Un programme qui lit la vitesse d'une voiture et s'elle dépasse la vitesse 80km, affiche un message, disant qu'il doit payer une amende
+vitesse = int(input("Quelle est la vitesse du véhicule? "))
