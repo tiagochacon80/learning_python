890912f2c7c396b1dcdbfbdae97df539b47076db
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-12 17:45:06 -0500

    defi029 - Photo radar

--- Conditions - if/defi029-photo_radar.py
+++ Conditions - if/defi029-photo_radar.py
@@ -1,2 +1,7 @@
 #Un programme qui lit la vitesse d'une voiture et s'elle dépasse la vitesse 80km, affiche un message, disant qu'il doit payer une amende
 vitesse = int(input("Quelle est la vitesse du véhicule? "))
+if vitesse > 80:
+    print("ATTENTION!! Vous avez dépassé la limite de vitesse permi qui est de 80km!!!")
+    print("Vous devez payer une amande!")
+else:
+    print("Bonne journée, conduisez prudemment")
