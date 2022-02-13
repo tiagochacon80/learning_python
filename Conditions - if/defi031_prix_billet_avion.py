aec6024a6822d60d2c7777c3556aeb38545b4f45
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-13 14:44:23 -0500

    defi033 -  Prix billet d'avion

--- Conditions - if/defi031_prix_billet_avion.py
+++ Conditions - if/defi031_prix_billet_avion.py
@@ -1,2 +1,9 @@
 #Programme qui demande une distance d'un voyage en km et calcule le prix du billet d'avion, les couts sont 0.50$/km jusqu'à 200km et 0.45$ pour les voyages plus long
-distance = int(input("Entrez la distance de votre voyage: "))
+distance = float(input("Entrez la distance de votre voyage: "))
+prix = 0
+if distance <= 200:
+    prix = distance * 0.50
+    print("Le prix de billet coûtera {:.2f}$".format(prix))
+if distance > 200:
+    prix = distance * 0.45
+    print("Le prix de billet coûtera {:.2f}$".format(prix))
