cd427084c84da5ceedc1dd8c39666d0ccdb876d0
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-14 20:33:10 -0500

    defi032 - année bissextile

---
+++ Conditions - if/defi032_annee_bissextile.py
@@ -0,0 +1,6 @@
+#Faites un programme qui lit n'importe quelle année et indique s'il s'agit d'une année bissextile.
+annee = int(input("Entrez un année: "))
+if (annee % 4 == 0 and annee % 100 != 0) or (annee % 400 == 0):
+    print("La année de {} est bissextile".format(annee))
+else:
+    print("L'année {} n'est pas bissextile".format(annee))
