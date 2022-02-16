8c7c86aa0a55d239aaf4ac0ba75156450eef4a27
Tiago Chacon <tiagochacon80@gmail.com>
2022-02-15 20:36:19 -0500

    Projets - jeu de devinette

--- Projets/jogo_adivinhacao.py
+++ Projets/jogo_adivinhacao.py
@@ -4,7 +4,7 @@ print("Bienvenu au jeu de devinette!")
 print("*********************************")
 
 nombre_secret = randint(1, 100)
-tentativas = 4
+tentatives = 4
 points = 1000
 
 print("Voici les niveaux de difficultés")
@@ -13,14 +13,14 @@ print("(1) Facile (2) Moderé (3) difficile")
 niveau = int(input("Choisissez votre niveau: "))
 
 if niveau == 1:
-    tentativas = 8
+    tentatives = 8
 elif niveau == 2:
-    tentativas = 5
+    tentatives = 5
 else:
-    tentativas = 3
+    tentatives = 3
 
-for rodada in range(1, tentativas + 1):
-    print("Rodada {} de {}".format(rodada, tentativas))
+for tour in range(1, tentatives + 1):
+    print("Rodada {} de {}".format(tour, tentatives))
     chute = int(input("Digite o seu numero entre 1 e 100: "))
     if(chute < 1 or chute > 100):
        print("Numero invalido, digite de 1 a 100")
@@ -40,6 +40,6 @@ for rodada in range(1, tentativas + 1):
         points_perdu = abs(nombre_secret - chute)
         points = points - points_perdu
 
-    rodada = rodada + 1
+    tour = tour + 1
 
     print("Fim do jogo, voce esgotou todas as tentativas")
