874df1cbf7d928d95ec3bd79242ae996b912d199
Tiago Chacon <tiagochacon80@gmail.com>
2022-03-12 18:23:57 -0500

    jeu devinette

--- Projets/jogo_adivinhacao.py
+++ Projets/jogo_adivinhacao.py
@@ -1,50 +1,50 @@
-from random import randint
+import random
 
-print("Bienvenu au jeu de devinette!")
-print("*********************************")
+def jouer():
 
-nombre_secret = randint(1, 100)
-tentatives = 0
-points = 1000
+    print("Bienvenu au jeu de devinette!")
+    print("*********************************")
 
-print("Voici les niveaux de difficultés")
-print("(1) Facile (2) Moderé (3) difficile")
+    nombre_secret = random.randint(1, 100)
+    tentativas = 4
+    points = 1000
 
-niveau = int(input("Choisissez votre niveau: "))
+    print("Voici les niveaux de difficultés")
+    print("(1) Facile (2) Moderé (3) difficile")
 
-if niveau == 1:
-    tentatives = 8
-elif niveau == 2:
-    tentatives = 5
-else:
-    tentatives = 3
+    niveau = int(input("Choisissez votre niveau: "))
 
-for tour in range(1, tentatives + 1):
-    print("Tour {} de {}".format(tour, tentatives))
-
-    chute = int(input("Entrez un nombre entre 1 et 100: "))
-
-    if(chute < 1 or chute > 100):
-        print("Nombre invalide, tapez de 1 a 100")
-        continue
-
-    acertou = chute == nombre_secret
-    maior = chute > nombre_secret
-    menor = chute < nombre_secret
-
-    if(acertou):
-        print("Parabéns voce acertou e fez {} pontos".format(points))
-        break
+    if niveau == 1:
+        tentativas = 8
+    elif niveau == 2:
+        tentativas = 5
     else:
-        points_perdu = abs(nombre_secret - chute)
-        points = points - points_perdu
-        if(menor):
-            print("Errou!!! O numero é maior que", chute)
-            if tour == tentatives:
-                print("Le nombre secret était {}. Votre total des points est de {}".format(nombre_secret, points))
-        elif(maior):
-            print("Errou!!! O numero é menor que", chute)
-            if tour == tentatives:
-                print("Le nombre secret était {}. Votre total des points est de {}".format(nombre_secret, points))
+        tentativas = 3
 
-print("Fin du jeu, vous avez épuisés toutes les tentatives")
+    for rodada in range(1, tentativas + 1):
+        print("Rodada {} de {}".format(rodada, tentativas))
+
+        chute = int(input("Digite o seu numero entre 1 e 100: "))
+        if(chute < 1 or chute > 100):
+           print("Numero invalido, digite de 1 a 100")
+           continue
+
+        acertou = chute == nombre_secret
+        maior = chute > nombre_secret
+        menor = chute < nombre_secret
+
+        if(acertou):
+            print("Parabéns voce acertou e fez {} pontos".format(points))
+            break
+        else:
+            if(menor):
+                print("Errou!!! O numero é maior que", chute)
+            elif(maior):
+                print("Errou!!! O numero é menor que", chute)
+            points_perdu = abs(nombre_secret - chute)
+            points = points - points_perdu
+
+    print("Fim do jogo, voce esgotou todas as tentativas")
+
+if(__name__== "__main__"):
+    jouer()
